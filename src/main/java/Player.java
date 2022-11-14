52cd572993e9155216e7ed4890c467cafa750bb0
Magnus Ottosen <magnus@ottosen.dk>
2022-11-14 14:09:08 +0100

    Update Player.java

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -5,6 +5,12 @@ public class Player {
     Die die1 = new Die();
     Die die2 = new Die();
 
+    private int position = 0;
+
+    private boolean inJail = false;
+
+    private int getOutOfJail = 0;
+
     public void rollDice(){
         die1.rollDie();
         die2.rollDie();
@@ -12,5 +18,26 @@ public class Player {
 
     public int sumOfDice(){
         return die1.getFaceValue()+die2.getFaceValue();
+    }
+
+    public boolean useGetOutOfJailCard(){
+        if(getOutOfJail>0){
+            getOutOfJail--;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean hasGetOutOfJailCard(){
+        if(getOutOfJail>0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean isInJail(){
+        return inJail;
     }
 }
